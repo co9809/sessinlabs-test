28a8c34ce8f4d516f69aee457d5f54fb4cdea067
rokawara <rokawara@gnext.co.jp>
2017-10-28 22:27:41 +0900

    fix

---
+++ Logic_1.php
@@ -0,0 +1,63 @@
+<?php
+
+class PhpMdTest
+{
+    public function test_function($param_string)
+    {
+        print "Hello sdfsfsdf!!" . $param_string . "\n";
+    }
+}
+
+
+if (!isset($aryIssueEst['issue_aaaestimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+
+
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
+if (!isset($aryIssueEst['issue_estimate_sending_time']) || strlen($aryIssueEst['issue_estimate_sending_time'])
+    < 1) {
+    $aryEstimate['issue_estimate_sending_time'] = $time;
+} else {
+    $result['dateTime'] .= ' '.date('H:i',
+            strtotime($aryIssueEst['issue_estimate_sending_time']));
+}
---
+++ Logic_2.php
@@ -0,0 +1,9 @@
+<?php
+
+class PhpMdTest
+{
+    public function test_function($param_string)
+    {
+        print "Hellowあああああああああああああああああ World!!" . $param_string . "\n";
+    }
+}
